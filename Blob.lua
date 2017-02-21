314b519766b330aaf4ed282979772960f55df3a6
Moritz Neikes <moritz@post-apocalyptic-crypto.org>
2017-02-21 13:28:40 +0100

    Fix capitalization typo

--- Blob.lua
+++ Blob.lua
@@ -9,7 +9,7 @@ if string.packsize and string.unpack then
     if bytes then return string.format("s%d", bytes)
     else return "s" end
   end
-  blob.backend = "lua"
+  Blob.backend = "lua"
 else
   local struct = require("struct")
   lib.size = struct.size
@@ -19,7 +19,7 @@ else
     if bytes then return string.format("I%dc0", bytes)
     else return "%Tc0" end
   end
-  blob.backend = "struct"
+  Blob.backend = "struct"
 end
 
 local unpack = unpack or table.unpack
